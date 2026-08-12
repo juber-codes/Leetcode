6c77264e68cabbc8e8f35006d11e357972e0ea96
Juber Atar <juber.atar.3009@gmail.com>
2026-08-12 19:31:55 +0530

    Time: 0 ms (100%), Space: 42.3 MB (21.71%) - LeetHub

---
+++ 0476-number-complement/0476-number-complement.java
@@ -0,0 +1,14 @@
+class Solution {
+    public int findComplement(int n) {
+
+        int mask = 0;
+        int temp = n;
+
+        while (temp > 0) {
+            mask = (mask << 1) | 1;
+            temp = temp >> 1;
+        }
+
+        return n ^ mask;
+    }
+}
