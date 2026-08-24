2554080778802aab018f3209cef5b09756cc9b39
Juber Atar <juber.atar.3009@gmail.com>
2026-08-24 22:42:08 +0530

    Time: 0 ms (100%), Space: 42.5 MB (53.3%) - LeetHub

--- 1185-day-of-the-week/1185-day-of-the-week.java
+++ 1185-day-of-the-week/1185-day-of-the-week.java
@@ -3,11 +3,44 @@ import java.time.format.TextStyle;
 import java.util.Locale;
 
 class Solution {
+    class weekDay{
+        int day = this.day;
+        int month = this.month;
+        int year = this.year;
+    }
     public String dayOfTheWeek(int day, int month, int year) {
+        String[] str = {"Sunday","Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"};
 
-        LocalDate date = LocalDate.of(year, month, day);
+        int[] daysM = {0,31,28,31,30,31,30,31,31,30,31,30,31};
 
-        return date.getDayOfWeek()
-                   .getDisplayName(TextStyle.FULL, Locale.ENGLISH);
+        // Check for the leapyear
+        if(leapYear(year)){
+            daysM[2] = 29;
+        }
+
+        int dayYear = 4;
+
+        for(int i = 1971; i < year; i++){
+            if(leapYear(i)){
+                dayYear += 366;
+            } else{
+                dayYear += 365;
+            }
+        }
+
+
+        for(int i = 1; i < month; i++){
+            dayYear += daysM[i];
+        }
+        dayYear += day; // cur day
+
+        return str[dayYear % 7];        
+    }
+
+    private boolean leapYear(int year){
+        if((year % 4 == 0 && year % 100 != 0) || (year % 400 == 0)){
+            return true;
+        }
+        return false;
     }
 }
